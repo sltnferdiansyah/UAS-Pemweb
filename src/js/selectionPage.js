1d070b951ee1ef461dd42e2ae74175982593d630
Sultan A Ferdiansyah <sultanferdiansyach@gmail.com>
2026-08-19 20:55:08 +0700

    add materi1

--- src/js/selectionPage.js
+++ src/js/selectionPage.js
@@ -1,13 +1,33 @@
 import { animationHandler } from "./helper/animateHelper";
 import { reactiveHelper } from "./helper/reactiveHelper";
+import materi1 from "../html/course/materi1.html?raw";
 
 export function selectionPageJS(router) {
     const homeButton = document.querySelector(".home-btn");
+    const courseContent = document.querySelector(".windows-content");
     let isDebouncePageOnly = false;
+
+    const domParser = (html) => {
+        const parser = new DOMParser();
+        return parser.parseFromString(html, "text/html");
+    };
+
+    const htmlPage = (html) => {
+        const parsedDocument = domParser(html);
+
+        courseContent.replaceChildren(...parsedDocument.body.childNodes);
+    };
+
 
     reactiveHelper.whenDOMLoaded(() => {
         animationHandler.addIn(1, "anim-1", "animate-out-1s", 0, 0, false);
     })
+
+    reactiveHelper.bindFormRadio("course-menu", (selectedId) => {
+        if (selectedId === "materi1") {
+            htmlPage(materi1);
+        }
+    }, 250);
 
     homeButton.addEventListener("click", () => {
         if (!isDebouncePageOnly) {
